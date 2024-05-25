5239eef565d1d50c6c86b1c52faf58c44eb64b41
Matheus Souza <Matheuszebende6@gmail.com>
2024-05-25 15:28:37 -0300

    Update questions.cy.ts

--- front-end/cypress/e2e/questions.cy.ts
+++ front-end/cypress/e2e/questions.cy.ts
@@ -1,6 +1,6 @@
 describe('Quiz Application Test', () => {
     it('should login and complete the quiz successfully', () => {
-      cy.visit('http://localhost:5173')
+      cy.visit('https://rocket-x-orpin.vercel.app/')
   
       cy.get('.start-btn').should('have.css', 'background-color', 'rgb(128, 128, 128)')
       cy.get('.card-01').click()
